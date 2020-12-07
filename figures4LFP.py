cb4782907c045771bed09a651510de897fec87d0
jduprez <55353429+jduprez@users.noreply.github.com>
2020-12-07 09:16:54 +0100

    Add files via upload

---
+++ figures4LFP.py
@@ -0,0 +1,210 @@
+import numpy as np
+import scipy.io
+import scipy as sp
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.offline import plot
+import pandas as pd
+import math
+import orca
+import seaborn as sns
+
+# Get path of the data to be used
+
+    inpath = '/your_path/'
+    outpath = 'your_output_path'
+
+## ----------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------
+## BEHAVIOR
+# load the data
+
+LFPdat = pd.read_csv(inpath + '/behavior_data.txt', sep="\t", header=0)
+
+ax = [None] * (2 + 2)
+fig = plt.figure(figsize=(5, 3.5))
+
+gs = mpl.gridspec.GridSpec(nrows=1,
+                           ncols=1,
+                           figure=fig,
+                           wspace=0.25, hspace=0.25
+                           )
+ax[0] = fig.add_subplot(gs[0, 0])
+sns.set_palette('Paired')
+sns.violinplot(x="emotion", y="accuracy", hue='task', data=LFPdat, split=True)
+
+sns.swarmplot(x="emotion", y="accuracy", hue='task', data=LFPdat, dodge=True,
+              color="black", size=3, edgecolor='white', linewidth=0.5, alpha=0.5)
+ax[0].set_ylabel('Accuracy rate')
+#ax[0].legend().set_visible(False)
+ax[0].set_xlabel('')
+
+leg = plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01),
+            borderaxespad=0, frameon=False, fontsize=8, ncol =2)
+
+leg.get_texts()[0].set_text('Gender task')
+leg.get_texts()[1].set_text('Emotion task')
+leg.legendHandles[2].set_visible(False)
+leg.legendHandles[3].set_visible(False)
+
+leg.get_texts()[2].set_text('')
+leg.get_texts()[3].set_text('')
+
+ax[0].xaxis.set_ticklabels(['Neutral', 'Fear'])
+
+fig.savefig(outpath + '/accuracy_violin_split.png',
+            format='png', dpi=1000)
+
+
+## ----------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------
+# Power
+# load the data
+
+LFPdat = pd.read_csv(inpath + '/statfile.txt', sep="\t", header=0)
+
+# Plot
+
+ax = [None] * (2 + 2)
+fig = plt.figure(figsize=(10, 7))
+
+gs = mpl.gridspec.GridSpec(nrows=2,
+                           ncols=2,
+                           figure=fig,
+                           wspace=0.25, hspace=0.25
+                           )
+ax[0] = fig.add_subplot(gs[0, 0])
+sns.set_palette('Paired')
+sns.violinplot(x="emo", y="pwr", hue='task', data=LFPdat[LFPdat.freq == 'delta1'], split=True)
+
+sns.swarmplot(x="emo", y="pwr",hue='task', data=LFPdat[LFPdat.freq == 'delta1'], dodge=True,
+              color="black", size=3, edgecolor='white', linewidth=0.5, alpha=0.5)
+ax[0].set_ylabel('delta 1 power (dB)')
+#ax[0].legend().set_visible(False)
+ax[0].set_xlabel('')
+ax[0].set_ylim([-12, 12])
+# ax[0].collections[0].set_facecolor('tab:blue')
+# ax[0].collections[1].set_facecolor('lightsteelblue')
+# ax[0].collections[3].set_facecolor('tab:blue')
+# ax[0].collections[4].set_facecolor('lightsteelblue')
+leg = plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01),
+            borderaxespad=0, frameon=False, fontsize=8, ncol =2)
+
+leg.get_texts()[0].set_text('Gender task')
+leg.get_texts()[1].set_text('Emotion task')
+leg.legendHandles[2].set_visible(False)
+leg.legendHandles[3].set_visible(False)
+
+leg.get_texts()[2].set_text('')
+leg.get_texts()[3].set_text('')
+
+ax[0].xaxis.set_ticklabels(['Neutral', 'Fear'])
+ax[0].text(-0.95, 14, 'A', fontsize=16, fontweight='bold')
+
+
+ax[1] = fig.add_subplot(gs[0, 1])
+sns.violinplot(x="emo", y="pwr", hue='task', data=LFPdat[LFPdat.freq == 'delta2'], split=True)
+sns.swarmplot(x="emo", y="pwr",hue='task', data=LFPdat[LFPdat.freq == 'delta2'], dodge=True,
+              color="black", size=3, edgecolor='white', linewidth=0.5, alpha=0.5)
+ax[1].set_ylabel('delta 2 power (dB)')
+ax[1].legend().set_visible(False)
+ax[1].set_xlabel('')
+ax[1].set_ylim([-12, 12])
+# ax[1].collections[0].set_facecolor('tab:blue')
+# ax[1].collections[1].set_facecolor('lightsteelblue')
+# ax[1].collections[3].set_facecolor('tab:blue')
+# ax[1].collections[4].set_facecolor('lightsteelblue')
+ax[1].xaxis.set_ticklabels(['Neutral', 'Fear'])
+ax[1].text(-0.95, 14, 'B', fontsize=16, fontweight='bold')
+
+ax[2] = fig.add_subplot(gs[1, 0])
+sns.violinplot(x="emo", y="pwr", hue='task', data=LFPdat[LFPdat.freq == 'beta'], split=True)
+sns.swarmplot(x="emo", y="pwr",hue='task', data=LFPdat[LFPdat.freq == 'beta'], dodge=True,
+              color="black", size=3, edgecolor='white', linewidth=0.5, alpha=0.5)
+ax[2].set_ylabel('alpha-beta power (dB)')
+ax[2].set_xlabel('')
+ax[2].legend().set_visible(False)
+ax[2].set_ylim([-12, 12])
+# ax[2].collections[0].set_facecolor('tab:blue')
+# ax[2].collections[1].set_facecolor('lightsteelblue')
+# ax[2].collections[3].set_facecolor('tab:blue')
+# ax[2].collections[4].set_facecolor('lightsteelblue')
+ax[2].xaxis.set_ticklabels(['Neutral', 'Fear'])
+ax[2].text(-0.95, 14, 'C', fontsize=16, fontweight='bold')
+
+fig.savefig(outpath + '/power_violin_split.png',
+            format='png', dpi=1000)
+
+
+## ----------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------
+# Same with ITPC
+# load the data
+
+LFPdat = pd.read_csv(inpath + 'statfile_itpc.txt', sep="\t", header=0)
+
+# Plot
+
+ax = [None] * (2 + 2)
+fig = plt.figure(figsize=(10, 3.5))
+
+gs = mpl.gridspec.GridSpec(nrows=1,
+                           ncols=2,
+                           figure=fig,
+                           wspace=0.25, hspace=0.25
+                           )
+ax[0] = fig.add_subplot(gs[0, 0])
+sns.set_palette('Paired')
+sns.violinplot(x="emo", y="pwr", hue='task', data=LFPdat[LFPdat.freq == 'delta1'], split=True)
+
+sns.swarmplot(x="emo", y="pwr",hue='task', data=LFPdat[LFPdat.freq == 'delta1'], dodge=True,
+              color="black", size=3, edgecolor='white', linewidth=0.5, alpha=0.5)
+ax[0].set_ylabel('delta ITPCz')
+#ax[0].legend().set_visible(False)
+ax[0].set_xlabel('')
+#ax[0].set_ylim([-12, 12])
+# ax[0].collections[0].set_facecolor('tab:blue')
+# ax[0].collections[1].set_facecolor('lightsteelblue')
+# ax[0].collections[3].set_facecolor('tab:blue')
+# ax[0].collections[4].set_facecolor('lightsteelblue')
+leg = plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01),
+            borderaxespad=0, frameon=False, fontsize=8, ncol =2)
+
+leg.get_texts()[0].set_text('Gender task')
+leg.get_texts()[1].set_text('Emotion task')
+leg.legendHandles[2].set_visible(False)
+leg.legendHandles[3].set_visible(False)
+
+leg.get_texts()[2].set_text('')
+leg.get_texts()[3].set_text('')
+
+ax[0].xaxis.set_ticklabels(['Neutral', 'Fear'])
+ax[0].set(ylim =(-200, 800))
+ax[0].text(-0.95, 850, 'A', fontsize=16, fontweight='bold')
+
+
+ax[1] = fig.add_subplot(gs[0, 1])
+sns.violinplot(x="emo", y="pwr", hue='task', data=LFPdat[LFPdat.freq == 'theta'], split=True)
+sns.swarmplot(x="emo", y="pwr",hue='task', data=LFPdat[LFPdat.freq == 'theta'], dodge=True,
+              color="black", size=3, edgecolor='white', linewidth=0.5, alpha=0.5)
+ax[1].set_ylabel('theta  ITPCz ')
+ax[1].legend().set_visible(False)
+ax[1].set_xlabel('')
+#ax[1].set_ylim([-12, 12])
+# ax[1].collections[0].set_facecolor('tab:blue')
+# ax[1].collections[1].set_facecolor('lightsteelblue')
+# ax[1].collections[3].set_facecolor('tab:blue')
+# ax[1].collections[4].set_facecolor('lightsteelblue')
+ax[1].xaxis.set_ticklabels(['Neutral', 'Fear'])
+ax[1].set(ylim =(-200, 800))
+ax[1].text(-0.75, 850, 'B', fontsize=16, fontweight='bold')
+
+fig.savefig(outpath + '/itpc_violin_split.png',
+            format='png', dpi=1000)
+
---
+++ stats.R
@@ -0,0 +1,381 @@
+
+# The following code has been used to perform the statistical analyses on the data presented in the paper:
+# Low-frequency subthalamic neural oscillations are involved in explicit and implicit facial emotional processing - a local field potential study, (Duprez et al., XXXX)
+
+# This scripts needs the {lme4},{car}, {multcomp} and {MuMin} packages
+
+library(c('lme4', 'MuMin', 'car', 'multcomp'))
+
+## Mixed models on behavior only - all the data
+
+data = read.table(paste("yourpath/behavior.txt"), header=TRUE, sep = "\t")
+
+data$emotion = as.factor(data$emotion)
+data$task = as.factor(data$task)
+
+moddacc = glmer(accuracy~emotion*task+(1|n), family=binomial(link=logit), data=data, glmerControl(optimizer = "bobyqa"))
+moddacc2 = glmer(accuracy~emotion*task+(emotion|n), family=binomial(link=logit), data=data, glmerControl(optimizer = "bobyqa")) # this one
+moddacc3 = glmer(accuracy~emotion*task+(task|n), family=binomial(link=logit), data=data, glmerControl(optimizer = "bobyqa"))
+moddacc4 = glmer(accuracy~emotion*task+(emotion|n)+(task|n), family=binomial(link=logit), data=data, glmerControl(optimizer = "bobyqa"))
+
+Anova(moddacc2)
+
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(moddacc2), residuals(moddacc2),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(moddacc2), residuals(moddacc2)))
+
+r.squaredGLMM(moddacc2)
+
+with(data, aggregate(accuracy, list(data$emotion, data$task), mean))
+with(data, aggregate(accuracy, list(data$emotion, data$task), sd))
+
+
+## Power analyses
+
+dataLFP = read.table(paste("yourpath/statfile_trial.txt"), header=TRUE, sep = "\t")
+
+dataLFP$emo = as.factor(dataLFP$emo)
+dataLFP$task = as.factor(dataLFP$task)
+dataLFP$nucleus = as.factor(dataLFP$nucleus)
+
+## Analyses for delta1
+
+
+require(lme4)
+modd1 = lmer(pwr~emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd2 = lmer(pwr~emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd3 = lmer(pwr~emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd4 = lmer(pwr~emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),]) 
+modd5 = lmer(pwr~emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),]) # THIS ONE !
+modd6 = lmer(pwr~emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd7 = lmer(pwr~emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd8 = lmer(pwr~emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd9 = lmer(pwr~emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "delta1"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+# Get mean and sd
+with(dataLFP2[which(dataLFP2$freq == "delta1"),], aggregate(pwr, list(dataLFP2[which(dataLFP2$freq == "delta1"),]$emo, dataLFP2[which(dataLFP2$freq == "delta1"),]$task), mean))
+with(dataLFP2[which(dataLFP2$freq == "delta1"),], aggregate(pwr, list(dataLFP2[which(dataLFP2$freq == "delta1"),]$emo, dataLFP2[which(dataLFP2$freq == "delta1"),]$task), sd))
+
+Anova(modd5)
+
+# Graphical check of the model's assumptions
+residus<-residuals(modd5)
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd5), residuals(modd5),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd5), residuals(modd5)))
+
+# R squared 
+r.squaredGLMM(modd5)
+
+## Analyses for delta2
+
+modd1 = lmer(pwr~emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd2 = lmer(pwr~emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd3 = lmer(pwr~emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd4 = lmer(pwr~emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd5 = lmer(pwr~emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),]) # THIS ONE !
+modd6 = lmer(pwr~emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd7 = lmer(pwr~emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),]) 
+modd8 = lmer(pwr~emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),]) 
+modd9 = lmer(pwr~emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "delta2"),]) 
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+with(dataLFP2[which(dataLFP2$freq == "delta2"),], aggregate(pwr, list(dataLFP2[which(dataLFP2$freq == "delta2"),]$emo, dataLFP2[which(dataLFP2$freq == "delta2"),]$task), mean))
+with(dataLFP2[which(dataLFP2$freq == "delta2"),], aggregate(pwr, list(dataLFP2[which(dataLFP2$freq == "delta2"),]$emo, dataLFP2[which(dataLFP2$freq == "delta2"),]$task), sd))
+
+Anova(modd5, test.statistic = "F")
+residus<-residuals(modd5)
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd5), residuals(modd5),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd5), residuals(modd5)))
+
+r.squaredGLMM(modd5)
+
+# Posthoc analyses
+dataLFP$inter = interaction(dataLFP$emo, dataLFP$task) # Create an interaction variable
+posthoc = lmer(pwr~inter +(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+
+postdelta2 = glht(posthoc,linfct=mcp(inter="Tukey"))
+summary(postdelta2)
+
+## Analyses for beta
+
+
+require(lme4)
+modd1 = lmer(pwr~emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd2 = lmer(pwr~emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd3 = lmer(pwr~emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd4 = lmer(pwr~emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd5 = lmer(pwr~emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "beta"),]) # THIS ONE !
+modd6 = lmer(pwr~emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd7 = lmer(pwr~emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "beta"),]) 
+modd8 = lmer(pwr~emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "beta"),]) 
+modd9 = lmer(pwr~emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "beta"),]) 
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+with(dataLFP2[which(dataLFP2$freq == "beta"),], aggregate(pwr, list(dataLFP2[which(dataLFP2$freq == "beta"),]$emo, dataLFP2[which(dataLFP2$freq == "beta"),]$task), mean))
+with(dataLFP2[which(dataLFP2$freq == "beta"),], aggregate(pwr, list(dataLFP2[which(dataLFP2$freq == "beta"),]$emo, dataLFP2[which(dataLFP2$freq == "beta"),]$task), sd))
+
+Anova(modd5)
+residus<-residuals(modd5)
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd5), residuals(modd5),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd5), residuals(modd5)))
+
+
+r.squaredGLMM(modd5)
+
+
+
+## Same for ITPC
+
+dataLFP = read.table(paste("yourpath/statfile_itpc.txt"), header=TRUE, sep = "\t")
+
+dataLFP$emo = as.factor(dataLFP$emo)
+dataLFP$task = as.factor(dataLFP$task)
+dataLFP$nucleus = as.factor(dataLFP$nucleus)
+
+## Analyses for delta1
+
+modd1 = lmer(pwr~emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd2 = lmer(pwr~emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd3 = lmer(pwr~emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd4 = lmer(pwr~emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd5 = lmer(pwr~emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd6 = lmer(pwr~emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])# THIS ONE
+modd7 = lmer(pwr~emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd8 = lmer(pwr~emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd9 = lmer(pwr~emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "delta1"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+with(dataLFP[which(dataLFP$freq == "delta1"),], aggregate(pwr, list(dataLFP[which(dataLFP$freq == "delta1"),]$emo, dataLFP[which(dataLFP$freq == "delta1"),]$task), mean))
+with(dataLFP[which(dataLFP$freq == "delta1"),], aggregate(pwr, list(dataLFP[which(dataLFP$freq == "delta1"),]$emo, dataLFP[which(dataLFP$freq == "delta1"),]$task), sd))
+
+Anova(modd6, test.statistic = 'F')
+residus<-residuals(modd6)
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd5), residuals(modd6),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd6), residuals(modd6)))
+
+
+r.squaredGLMM(modd6)
+
+
+
+## Analyses for theta
+
+modd1 = lmer(pwr~emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd2 = lmer(pwr~emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd3 = lmer(pwr~emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd4 = lmer(pwr~emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd5 = lmer(pwr~emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])# THIS ONE
+modd6 = lmer(pwr~emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "theta"),]) 
+modd7 = lmer(pwr~emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd8 = lmer(pwr~emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd9 = lmer(pwr~emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "theta"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+with(dataLFP[which(dataLFP$freq == "theta"),], aggregate(pwr, list(dataLFP[which(dataLFP$freq == "theta"),]$emo, dataLFP[which(dataLFP$freq == "theta"),]$task), mean))
+with(dataLFP[which(dataLFP$freq == "theta"),], aggregate(pwr, list(dataLFP[which(dataLFP$freq == "theta"),]$emo, dataLFP[which(dataLFP$freq == "theta"),]$task), sd))
+
+
+Anova(modd5, test.statistic = 'F')
+residus<-residuals(modd5)
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd5), residuals(modd5),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd5), residuals(modd5)))
+
+
+r.squaredGLMM(modd5)
+
+## Associations between power/ITPC and behavior (accuracy)
+
+dataLFP = read.table(paste("yourpath/statfile_itpc.txt"), header=TRUE, sep = "\t")
+
+## Accuracy delta 1
+
+modd1 = lmer(accuracy~pwr+emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd2 = lmer(accuracy~pwr+emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd3 = lmer(accuracy~pwr+emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),])# THIS ONE !
+modd4 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),]) 
+modd5 = lmer(accuracy~pwr+emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd6 = lmer(accuracy~pwr+emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd7 = lmer(accuracy~pwr+emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd8 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd9 = lmer(accuracy~pwr+emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "delta1"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+with(dataLFP[which(dataLFP$freq == "delta1"),], aggregate(accuracy, list(dataLFP[which(dataLFP$freq == "delta1"),]$emo, dataLFP[which(dataLFP$freq == "delta1"),]$task), mean))
+with(dataLFP[which(dataLFP$freq == "delta1"),], aggregate(accuracy, list(dataLFP[which(dataLFP$freq == "delta1"),]$emo, dataLFP[which(dataLFP$freq == "delta1"),]$task), sd))
+
+Anova(modd3, test.statistic = "F")
+
+#can use redres package to inspect assumptions as well, only works with lme4 models
+#require(redres)
+#plot_redres(modd3)
+#plot_resqq(modd3)
+residus<-residuals(modd3)
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd3), residuals(modd3),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd3), residuals(modd3)))
+
+
+r.squaredGLMM(modd3)
+
+
+## Accuracy delta 2
+
+modd1 = lmer(accuracy~pwr+emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd2 = lmer(accuracy~pwr+emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd3 = lmer(accuracy~pwr+emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "delta2"),])# THIS ONE !
+modd4 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "delta2"),]) 
+modd5 = lmer(accuracy~pwr+emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd6 = lmer(accuracy~pwr+emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd7 = lmer(accuracy~pwr+emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd8 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta2"),])
+modd9 = lmer(accuracy~pwr+emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "delta2"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+with(dataLFP[which(dataLFP$freq == "delta2"),], aggregate(accuracy, list(dataLFP[which(dataLFP$freq == "delta2"),]$emo, dataLFP[which(dataLFP$freq == "delta2"),]$task), mean))
+with(dataLFP[which(dataLFP$freq == "delta2"),], aggregate(accuracy, list(dataLFP[which(dataLFP$freq == "delta2"),]$emo, dataLFP[which(dataLFP$freq == "delta2"),]$task), sd))
+
+Anova(modd3, test.statistic = "F")
+
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd3), residuals(modd3),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd3), residuals(modd3)))
+
+r.squaredGLMM(modd3)
+
+## Accuracy Beta
+
+modd1 = lmer(accuracy~pwr+emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd2 = lmer(accuracy~pwr+emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd3 = lmer(accuracy~pwr+emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "beta"),])# THIS ONE !
+modd4 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd5 = lmer(accuracy~pwr+emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd6 = lmer(accuracy~pwr+emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd7 = lmer(accuracy~pwr+emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd8 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "beta"),])
+modd9 = lmer(accuracy~pwr+emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "beta"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+Anova(modd3, test.statistic = "F")
+
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd3), residuals(modd3),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd3), residuals(modd3)))
+
+
+
+
+## Same for ITPC
+
+dataLFP = read.table(paste("yourpath/statfile_itpc.txt"), header=TRUE, sep = "\t")
+
+dataLFP$emo = as.factor(dataLFP$emo)
+dataLFP$task = as.factor(dataLFP$task)
+dataLFP$nucleus = as.factor(dataLFP$nucleus)
+
+# Delta
+
+modd1 = lmer(accuracy~pwr+emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd2 = lmer(accuracy~pwr+emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd3 = lmer(accuracy~pwr+emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),])# THIS ONE !
+modd4 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "delta1"),]) 
+modd5 = lmer(accuracy~pwr+emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd6 = lmer(accuracy~pwr+emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd7 = lmer(accuracy~pwr+emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd8 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "delta1"),])
+modd9 = lmer(accuracy~pwr+emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "delta1"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+
+Anova(modd3, test.statistic = "F")
+
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd3), residuals(modd3),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd3), residuals(modd3)))
+
+r.squaredGLMM(modd3)
+
+
+# Theta
+
+modd1 = lmer(accuracy~pwr+emo*task +(1|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd2 = lmer(accuracy~pwr+emo*task+nucleus+(1|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd3 = lmer(accuracy~pwr+emo*task +(emo|n), data=dataLFP[which(dataLFP$freq == "theta"),])# THIS ONE !
+modd4 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n), data=dataLFP[which(dataLFP$freq == "theta"),]) 
+modd5 = lmer(accuracy~pwr+emo*task +(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd6 = lmer(accuracy~pwr+emo*task+nucleus+(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd7 = lmer(accuracy~pwr+emo*task +(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd8 = lmer(accuracy~pwr+emo*task+nucleus+(emo|n)+(task|n), data=dataLFP[which(dataLFP$freq == "theta"),])
+modd9 = lmer(accuracy~pwr+emo*task + nucleus +(emo|n)+(task|n)+(n|nucleus), data=dataLFP[which(dataLFP$freq == "theta"),])
+
+anova(modd1, modd2, modd3, modd4, modd5, modd6, modd7, modd8, modd9) # Compare models and keep the one that converges with the lower AIC
+
+
+Anova(modd3, test.statistic = "F")
+
+qqnorm(residus)
+qqline(residus)
+
+plot(fitted(modd3), residuals(modd3),
+     xlab = "Fitted Values", ylab = "Residuals")
+abline(h=0, lty=2)
+lines(smooth.spline(fitted(modd3), residuals(modd3)))
+
+r.squaredGLMM(modd3)
+
+
+
